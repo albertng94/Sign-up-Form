c2991cc56e8c5b2f0c9330c5c31d546fd5da3cdf
albertng94 <albertng94@gmail.com>
2023-12-01 16:27:55 +0100

    Rename and Update function styleInvalidLabels (now styleInputElements)
    Renamed and updated the function:
    
    Now when an input is focused-out it will update both labels and the cue
    paragraphs.
    --> Labels will be colored red when the input content is invalid and
    white when it is valid.
    --> Cue paragraphs will be colored red when the input is invalid and
    transparent when it is valid.
    
    The function will also trigger when an input is focused-in updating
    cue paragraphs.
    --> Cue paragraphs will be colored white whenever they are focused-in
    and remain that way but only if they have a value, remaining transparent
    if not.
    
    The validation tests for the function are based on the input element's
    text-color, which in turn is validated by the RegEx used in HTML pattern
    attributes via CSS.

--- js/javascript.js
+++ js/javascript.js
@@ -29,28 +29,69 @@ confirmPassword.addEventListener("input", () => {
 });
 
 
-// Change the color of Labels to red when input value is invalid but not empty.
+// Style labels and cue paragraphs according to the current input status (empty, valid, or invalid)
 
 const labels = document.querySelectorAll("label");
 const inputs = document.querySelectorAll("input");
 
-function styleInvalidLabels() {
+function styleInputElements() {
     for (const input of inputs) {
         input.addEventListener("focusout", () => {
             const computedStyle = window.getComputedStyle(input);
             const inputColor = computedStyle.color;
-            console.log('Input color:', inputColor);
+
+            // Style labels and cue paragraphs red when input is invalid (color rgb(237, 94, 94))
 
             if (inputColor === "rgb(237, 94, 94)") {
                 input.parentNode.previousElementSibling.style.color = "rgb(237, 94, 94)";
-            } else {
+
+                if (input.id === "name" || input.id === "surname" || input.id === "phone") {
+                    input.parentNode.nextElementSibling.style.color = "rgb(237, 94, 94)";
+                } 
+                
+                else if (input.id === "password" || input.id === "password-confirm") {
+                    input.parentNode.nextElementSibling.nextElementSibling.style.color = "rgb(237, 94, 94)";
+                    input.parentNode.nextElementSibling.nextElementSibling.nextElementSibling.style.color = "rgb(237, 94, 94)";
+                }
+            } 
+            
+            // Style labels white and cue paragraphs transparent when input is valid (color rgb(98, 98, 97))
+
+            else if (inputColor === "rgb(98, 98, 97)") {
                 input.parentNode.previousElementSibling.style.color = "rgb(98, 98, 97)";
+
+                if (input.id === "name" || input.id === "surname" || input.id === "phone") {
+                    input.parentNode.nextElementSibling.style.color = "transparent";
+                }
+
+                else if (input.id === "password" || input.id === "password-confirm") {
+                    input.parentNode.nextElementSibling.nextElementSibling.style.color = "transparent";
+                    input.parentNode.nextElementSibling.nextElementSibling.nextElementSibling.style.color = "transparent";
+                }
+            }
+        });
+
+        // Style cue paragraphs white when the input field is the current focus and has some value.
+
+        input.addEventListener("focusin", () => {
+            const computedStyle = window.getComputedStyle(input);
+            const inputColor = computedStyle.color;
+
+            if (inputColor !== "rgb(237, 94, 94)" && input.value) {
+                if (input.id === "name" || input.id === "surname" || input.id === "phone") {
+                    input.parentNode.nextElementSibling.style.color = "rgb(196, 196, 196)";
+                } 
+                
+                else if (input.id === "password" || input.id === "password-confirm") {
+                    input.parentNode.nextElementSibling.nextElementSibling.style.color = "rgb(196, 196, 196)";
+                    input.parentNode.nextElementSibling.nextElementSibling.nextElementSibling.style.color = "rgb(196, 196, 196)";
+                }
             }
         });
     }
 }
 
-styleInvalidLabels();
+styleInputElements();
 
 
 // Unable to submit the form when passwords match pattern criteria but do not match
